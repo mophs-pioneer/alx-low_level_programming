6a0bf7f23b79e5cfeb6503226e4adc6c32f0b0fd
kabagi <kabagiphilip9708@gmail.com>
2023-03-16 15:06:59 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
 * main -this is the main function
-* return: always 0 (success)
+* Return: always 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
