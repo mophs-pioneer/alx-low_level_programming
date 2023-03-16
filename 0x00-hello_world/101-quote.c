ee81fe7a8bd34c9ebafcc081c59c46d977f73df8
kabagi <kabagiphilip9708@gmail.com>
2023-03-16 15:27:24 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
+#include<stdio.h>
 #include <unistd.h>
 
 /**
-* main -prints "and that place of art is useful " - Dora Korpar , 2015-10-19",
-*                   follovwed by a new line , to standard error.
+* main - A c program that prints a standard error
 *
 *Return: Always 1.
 */
 int  main(void)
 {
-write(2,
+fput(2,
 "and that place of art is useful\n" - Dorakorpar, 2015 - 10 - 19\n",
 59);
 
 return (1);
-]
+}
