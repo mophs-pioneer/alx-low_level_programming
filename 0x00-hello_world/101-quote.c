7f433066fdadbfff518242017489a26d228d1552
kabagi <kabagiphilip9708@gmail.com>
2023-03-16 14:20:47 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <unistd.h>
+
+/**
+* main -prints "and that place of art is useful " - Dora Korpar , 2015-10-19",
+*                   follovwed by a new line , to standard error.
+*
+*Return: Always 1.
+*/
+int  main(void)
+{
+write(2,
+"and that place of art is useful\n" - Dorakorpar, 2015 - 10 - 19\n",
+59);
+
+return (1);
+]
