43b80f7234fb06fb3605e4ea75259bf7b39e8f76
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 09:48:25 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,11 @@
 
 /**
 * main -Entry point
-*followed by a new line, to standard error.
-*
 *Return: Always 1.
 */
 int  main(void)
 {
-fputs(2,
+write(2,
 "and that place of art is useful\n" - Dorakorpar, 2015 - 10 - 19 \n",
 59);
 
