660cd71f4f1a7d216ba2257641acc455720dd13b
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 09:56:32 -0700

    p8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 */
 int  main(void)
 {
-write(2, "and that place of art is useful\" - Dorakorpar, 2015 - 10 - 19\n", 59);
+write(2, "and that place of art is useful\" - Dorakorpar, 2015 - 10 - 19\n", 
+59);
 return (1);
 }
