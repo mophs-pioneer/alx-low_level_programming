5a1d3790e52942f236a2948b8b831d0aec5c199d
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 16:56:48 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: Always o
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10 ; i++)
+	putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
