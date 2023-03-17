255364bb3ee1e649f8f405ecae3c23b51db8663b
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 09:44:12 -0700

    mophs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,15 @@
 
 /**
 * main -Entry point
+*followed by a new line, to standard error.
+*
 *Return: Always 1.
 */
 int  main(void)
 {
-write(2,"and that place of art is useful\" - Dorakorpar, 2015 - 10 - 19\n",59);
+fputs(2,
+"and that place of art is useful\n" - Dorakorpar, 2015 - 10 - 19 \n",
+59);
+
 return (1);
 }
