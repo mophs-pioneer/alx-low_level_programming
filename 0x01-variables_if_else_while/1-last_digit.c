439bf940b1bb553fda3847c1ac50de00949ecbaf
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 13:29:48 -0700

    mophs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ j = n % 10;
 if (j > 5)
 	printf("last digit of %d is %d and is greater than 5", n, j);
 else if (j == 0)
-	printf("last digit of %d is %d and is zero", n, j);
+	printf("last digit of %d is %d and is 0", n, j);
 else if (j < 6 && j != 0)
 	printf("last digit of %d is %d  and is less than 6 and not 0 ", n, j);
 printf("\n");
