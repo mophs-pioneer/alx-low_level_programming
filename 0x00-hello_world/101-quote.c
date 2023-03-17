0a619fd9e60d1acb2826c58fa86f788aff2ba319
kabagi <kabagiphilip9708@gmail.com>
2023-03-17 09:32:38 -0700

    no 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,11 @@
 #include <unistd.h>
 
 /**
-* main - "and that piece of art is useful" - Dorakorpar, 2015 - 10 - 19 ",
-*followed by a new line, to standard error.
-*
+* main -Entry point
 *Return: Always 1.
 */
 int  main(void)
 {
-fputs(2,
-"and that place of art is useful\n" - Dorakorpar, 2015 - 10 - 19\n",
-59);
-
+write(2,"and that place of art is useful\" - Dorakorpar, 2015 - 10 - 19\n",59);
 return (1);
 }
