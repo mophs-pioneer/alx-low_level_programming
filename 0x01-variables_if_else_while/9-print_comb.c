5529b1f524dba40502f823d7bb1b55ae5ff2908c
kabagi <kabagiphilip9708@gmail.com>
2023-03-18 05:30:25 -0700

    mm

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,18 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i < 10 ;i++)
+
+	for (i = 0 ; i < 10 ; i++)
+
 	{
 		if (i == 9)
 			putchar(i + '0');
 		else
 		{
-	         	putchar(i + '0');
-			putchar(',');
+	                putchar(i + '0');
+			putchar(','\t);
 	                putchar(' ');
-	}
+		}
 	}
 return (0);
 }
