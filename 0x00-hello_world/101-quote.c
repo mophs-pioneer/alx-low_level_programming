6b8087e789a2af8f29d97ac08e3093ca1e254716
kabagi <kabagiphilip9708@gmail.com>
2023-03-19 10:51:35 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
 /**
 * main -Entry point
 *Return: Always 1.
+*
 */
 int  main(void)
 {
 write(2,
-"and that place of art is useful\" - Dorakorpar, 2015 - 10 - 19\n", 59);
+"and that piece of art is useful\" - Dorakorpar, 2015 - 10 - 19\n", 59);
 return (1);
 }
