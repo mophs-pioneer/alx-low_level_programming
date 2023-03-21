bf6da9015b89a9b8cec30e5855f6a5a4b5a95aa8
kabagi <kabagiphilip9708@gmail.com>
2023-03-21 16:57:49 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,8 +27,8 @@ void print_to_98(int n)
 		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
-			 if (i != 98)
-                                 printf(", ");
+			               if (i != 98)
+	printf(", ");
 		}
 		printf("\n");
 	}
@@ -38,7 +38,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			 if (i != 98)
-                                  printf(", ");
+	printf(", ");
 		}
 		printf("\n");
 	}
