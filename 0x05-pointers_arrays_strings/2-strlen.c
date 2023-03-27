6c9f6be4338f30b9995f60059d83952e511464b2
kabagi <kabagiphilip9708@gmail.com>
2023-03-27 06:27:40 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen - returns string length
+* @s: string whose lengthy is to be printed
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0 ;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
