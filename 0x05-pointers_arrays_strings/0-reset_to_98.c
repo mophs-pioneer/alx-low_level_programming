2f58651fbd83550514bb9784ca31309c2655322f
kabagi <kabagiphilip9708@gmail.com>
2023-03-27 02:55:38 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - chnges the value of n to 98
+* @n: is the value for replacement
+* Return: always 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* reset_to_98 - changes of n to 98
+* @n: the value to be replaced
+* Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
