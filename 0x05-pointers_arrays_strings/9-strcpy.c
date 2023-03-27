25792dda6bfb7e62f2e0a8811f83172d83446fd1
kabagi <kabagiphilip9708@gmail.com>
2023-03-27 13:23:24 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strcpy - copies string
+* @dest: destinaion array
+* @src: source array
+* Return: destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (dest  == NULL)
+		return (NULL);
+	for (i = 0; src[i] != '\n' ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
