905c3f1a5145a6ca43ee6ed01dd99738801f7221
kabagi <kabagiphilip9708@gmail.com>
2023-04-05 14:21:44 -0700

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* factorial - returns the factorial of a number 
+* factorial - returns the factorial of a number
 * @n: number to return the factorial from
-* 
-* Return: factorial of n 
+*
+* Return: factorial of n
 */
 
 int factorial(int n)
