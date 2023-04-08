699d12bdfe31b65760cda0866d53f49273e844c3
kabagi <kabagiphilip9708@gmail.com>
2023-04-08 04:13:02 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - this prints all arguments it receives
+* @argc: this prints number of arguments
+* @argv: this is array of numbers
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int f;
+
+	for(f = 0; f < argc; f++)
+	{
+		printf("%s\n", argv[f]);
+	}
+	return (0);
+}
