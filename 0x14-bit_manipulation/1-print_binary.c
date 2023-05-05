231111886dad4bf7e7d67ae3bf2cab5bdcaa2460
mophs_pioneer <kabagiphilip9708@gmail.com>
2023-05-05 13:17:10 -0700

    crack password

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_binary - this is the function that prints the binary equivalent of a decimal number
+ * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
