a13adebc044f26b5b55e4f99fa9ef4656f8f149f
philip <lucymankware@gmail.com>
2023-08-17 08:32:07 -0700

    double list tsks

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - The function adds a new node at the end
+ * of a dlistint_t list
+ *
+ * @head: this the head of the list
+ * @n: the value of the element
+ * Return: the address of the new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h = *head;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+
+	new->prev = h;
+
+	return (new);
+}
